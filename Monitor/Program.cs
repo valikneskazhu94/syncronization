4e2a59620d977163d6c7e73cf3e6d7be01550b3e
valikneskazhu94 <valik_ne_skazhu@ukr.net>
2018-10-08 19:47:20 +0300

    Pisos

---
+++ Monitor/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//using System.Threading.Monitor;
+namespace Monitor1
+{
+    class InterLockedCounter
+    {
+        int field1;
+        int field2;
+        public int Field1
+        {
+            get {
+                return field1;
+            }
+        }
+        public int Field2
+        {
+            get
+            {
+                return field2;
+            }
+        }
+
+        public void UpdateFields()
+        {
+            for(int i=0;i<1000000;i++)
+            {
+
+                Interlocked.Increment(ref field1);
+                if(field1%2==0)
+                {
+                    Interlocked.Increment(ref field2);
+                }
+            }
+        }
+    }
+    class MonitorCounter
+    {
+        int field1;
+        int field2;
+        public int Field1
+        {
+            get
+            {
+                return field1;
+            }
+        }
+        public int Field2
+        {
+            get
+            {
+                return field2;
+            }
+        }
+
+        public void UpdateFields()
+        {
+            
+            try
+            {
+                Monitor.Enter(this);
+                for (int i = 0; i < 1000000; i++)
+                {
+
+                    Interlocked.Increment(ref field1);
+                    if (field1 % 2 == 0)
+                    {
+                        Interlocked.Increment(ref field2);
+                    }
+                }
+            }
+            finally
+            {
+                Monitor.Exit(this);
+            }
+        }
+    }
+    class Program
+    {
+        static void BadAsync()
+        {
+            Console.WriteLine("синхронизация Interlocked методами");
+            InterLockedCounter c = new InterLockedCounter();
+            Thread[] threads = new Thread[5];
+            for(int i=0;i<threads.Length;i++)
+            {
+                threads[i] = new Thread(c.UpdateFields);
+                threads[i].Start();
+            }
+        for(int i=0;i<threads.Length;i++)
+            {
+                threads[i].Join();
+            }
+            Console.WriteLine($"Fields: {c.Field1},{c.Field2}");
+        }
+        static void GoodAsync()
+        {
+            Console.WriteLine("синхронизация Monitor ");
+            MonitorCounter m = new MonitorCounter();
+            Thread[] threads = new Thread[5];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(m.UpdateFields);
+                threads[i].Start();
+            }
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+            Console.WriteLine($"Fields: {m.Field1},{m.Field2}");
+        }
+        static void Main(string[] args)
+        {
+            BadAsync();
+            GoodAsync();
+        }
+    }
+}
---
+++ synchronizationDEMO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace synchronizationDEMO
+{
+    class Counter
+    {
+        public static int count = 0;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[5];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(delegate () {
+                    for(int j=0;j<1000000;j++)
+                    {
+                        Interlocked.Increment(ref Counter.count);
+                    }
+                });
+            }
+
+            for(int i=0;i<threads.Length;i++)
+            {
+                threads[i].Start();
+            }
+
+            for(int i=0;i<threads.Length;i++)
+            {
+                threads[i].Join();
+            }
+            Console.WriteLine($"Counter = {Counter.count}");
+            Console.ReadKey();
+        }
+    }
+}
